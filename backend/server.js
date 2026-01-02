028f28f8426efdf787c54b2c91adbf1b30f215a0
Gamerking177 <rajaksh03@gmail.com>
2026-01-03 00:57:09 +0530

    cors setup done

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'DELETE'],
+}));
+
 app.use(express.json());
 
 //Routes
